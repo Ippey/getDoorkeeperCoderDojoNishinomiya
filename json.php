f2b270042c4b27d24dea42cfcb7e7e8d8eb36c9e
tkc49 <hosoya@communitylink.jp>
2015-04-17 18:46:50 +0900

    日本時間表記になるように変更

--- json.php
+++ json.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://api.doorkeeper.jp/groups/coderdojo-nishinomiya/events?locale=ja";
+$url = "http://api.doorkeeper.jp/groups/coderdojo-nishinomiya/events?since=2013-11-01T07:30:00.000Z&locale=ja";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($curl);
@@ -9,14 +9,14 @@ $events = array();
 if ($response) {
 	$json = json_decode($response, true);
 	foreach ($json as $row) {
-		$event = $row["event"];
+		$event = $row["event"];		
 		$event["public_url"] = preg_replace("/^https:/", "http:", $event["public_url"]);
-		$data = [
+		$data = array(
 			"title" => $event["title"],
-			"start" => $event["starts_at"],
-			"end" => $event["ends_at"],
+			"start" => getLocalDate( $event["starts_at"] ),
+			"end" => getLocalDate( $event["ends_at"] ),
 			"url" => $event["public_url"]
-		];
+		);
 		$events[] = $data;
 	}
 	header("Content-Type: application/json;charset=UTF-8");
@@ -24,4 +24,10 @@ if ($response) {
 
 }
 
-?>
+function getLocalDate( $date ){
+	
+	$localDate = new DateTime($date);
+	$localDate->setTimeZone(new DateTimeZone('Asia/Tokyo'));
+	
+	return $localDate->format('Y-m-d H:i:s');
+}
